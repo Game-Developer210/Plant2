d9ac95274bf1599712bff1b8bae2b5d684b0e37e
Game-Developer210 <gamedeveloper210@gmail.com>
2025-05-12 11:12:51 +0400

    Update getPlants.js

--- netlify/functions/getPlants.js
+++ netlify/functions/getPlants.js
@@ -1,10 +1,10 @@
 const fetch = require('node-fetch');
 
 exports.handler = async function (event, context) {
-  const token = process.env.REACT_APP_TREFLE_API_KEY;
+  //const token = process.env.REACT_APP_TREFLE_API_KEY;
 
   try {
-    const res = await fetch(`https://trefle.io/api/v1/plants?token=${token}&page_size=20`);
+    const res = await fetch("https://trefle.io/api/v1/plants?token=4sFVLc2wtQXu8CKFn_NycI0fE6YetBR9rBNGdD-sA6M&page_size=20");
     const data = await res.json();
 
     return {
